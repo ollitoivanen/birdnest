0d5ce46edf7ebdf644b1286ce4d9b96f6fcf1542
Olli Toivanen <olliteed@gmail.com>
2023-01-12 15:37:30 +0200

    Add fetch options constant to utils

--- api/src/utils/constants.js
+++ api/src/utils/constants.js
@@ -1,2 +1,7 @@
-const REAKTOR_DRONE_URL = "https://assignments.reaktor.com/birdnest/drones";
-const REAKTOR_Pilots_URL = "https://assignments.reaktor.com/birdnest/pilots/";
+const REAKTOR_DRONES_URL = "https://assignments.reaktor.com/birdnest/drones";
+const REAKTOR_PILOTS_URL = "https://assignments.reaktor.com/birdnest/pilots/";
+const FETCH_OPTIONS_GET = {
+  method: "GET",
+};
+
+module.exports = { REAKTOR_DRONES_URL, REAKTOR_PILOTS_URL, FETCH_OPTIONS_GET };

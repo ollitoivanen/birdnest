75953debe1a9b8c429f720451f1ea1d8d8712630
Olli Toivanen <olliteed@gmail.com>
2023-01-12 15:36:43 +0200

    Add test for xmlToArray helper function

--- api/src/tests/helpers.spec.js
+++ api/src/tests/helpers.spec.js
@@ -1,10 +1,20 @@
 const { expect } = require("chai");
 const fetchDroneDataFromReaktor = require("../helpers/fetchDroneDataFromReaktor");
+const xmlToArray = require("../helpers/xmlToArray");
+describe("Test helpers", () => {
+  describe("Test getDroneDataFromReaktor", () => {
+    it("Return an ok response", async () => {
+      const fetchDroneDataResult = await fetchDroneDataFromReaktor();
+      expect(fetchDroneDataResult.ok).to.equal(true);
+    });
+  });
 
-describe("Test getDroneDataFromReaktor", () => {
-  it("Return an ok response", async () => {
-    const actualResult = await fetchDroneDataFromReaktor();
-    console.log(actualResult);
-    expect(actualResult.ok).to.equal(true);
+  describe("Test xmlToArray", () => {
+    it("Return array", async () => {
+      const fetchDroneDataResult = await fetchDroneDataFromReaktor();
+      const actualResult = await xmlToArray(fetchDroneDataResult);
+
+      expect(Array.isArray(actualResult)).to.equal(true);
+    });
   });
 });

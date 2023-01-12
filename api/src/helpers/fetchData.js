4e4803f3db7301d86cafe9b2e91db26d2f225469
Olli Toivanen <olliteed@gmail.com>
2023-01-12 14:38:27 +0200

    Add fetchData helper function

---
+++ api/src/helpers/fetchData.js
@@ -0,0 +1,28 @@
+async function fetchData(url, options) {
+  // Node-included fetch api. Response from the Reaktor-provided endpoint.
+  let response;
+  try {
+    response = await fetch(url, options);
+  } catch (networkError) {
+    // Fetch promise only fails when there's a network error
+    // (e.g server is not connected to the internet).
+    handleNetworkError(networkError);
+    return;
+  }
+
+  if (!response?.ok) {
+    console.log("Response not OK.");
+    return;
+  }
+  return response;
+}
+
+function checkStatusCode(response) {
+  //Todo write responses to all status codes
+}
+
+function handleNetworkError(networkError) {
+  console.log(networkError);
+}
+
+module.exports = fetchData;

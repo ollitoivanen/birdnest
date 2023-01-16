ea6ab424e1626aa9b25643ed522577003ea4e20b
Olli Toivanen <olliteed@gmail.com>
2023-01-16 12:18:05 +0200

    Init Create-react-app

--- api/app.js
+++ api/app.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const mongoURL = require("./config");
+
 const violatorsRouter = require("./src/routes/violators");
+const getViolatorData = require("./src/services/getViolatorData");
+
+mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
-app.use(violatorsRouter);
+app.use("/violators", violatorsRouter);
+getViolatorData();
 module.exports = app;
